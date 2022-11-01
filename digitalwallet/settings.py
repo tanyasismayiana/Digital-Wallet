2df003a1168e8169b49c0f5647b0a80bf09239ec
Tanyasis <tanyamayiana.com>
2022-11-01 13:37:00 +0300

    delopy

--- digitalwallet/settings.py
+++ digitalwallet/settings.py
@@ -99,7 +99,7 @@ WSGI_APPLICATION = 'digitalwallet.wsgi.application'
 # }
 MODE=config("MODE", default="dev")
 SECRET_KEY = config('SECRET_KEY')
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = config('DEBUG', default=True, cast=bool)
 # development
 if config('MODE')=="dev":
    DATABASES = {
